9bb0cb27abf0de5d5262e734b729340f808d9d74
Oliver <olivecovski@gmail.com>
2020-07-03 17:45:59 +0700

    Initial commit

---
+++ database/migrations/2020_07_01_104906_create_destinations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDestinationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('destinations', function (Blueprint $table) {
+            $table->id();
+            $table->string('longitude');
+            $table->string('latitude');
+            $table->text('description');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('trip_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('destinations');
+    }
+}
