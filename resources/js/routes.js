cec2a0a5143623ff8ff2050e52a4fd54f015f476
Oliver <olivecovski@gmail.com>
2020-08-28 20:47:28 +0700

    Added route for provider Login

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -3,6 +3,7 @@ import Login from './components/Login/Login.vue';
 import Register from './components/Login/Register.vue';
 import TripList from './components/Trips/TripList';
 import Home from './components/Home';
+import LoginProvider from '.components/Login/LoginProvider'
 import store from './store/index';
 
 function redirectIfLoggedIn(to, from, next) {
